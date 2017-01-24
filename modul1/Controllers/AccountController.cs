52663d5350b67bc9bc29fbb35fe8807add3bd63c
Vejrynsh <Vejrynsh@DESKTOP-O4DVL5C>
2017-01-24 14:52:26 +0700

    Add project files.

---
+++ modul1/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using modul1.Models.ViewModel;
+using modul1.Models.EntityManager;
+
+namespace modul1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(UserSignUpView USV)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager UM = new UserManager();
+                if (!UM.IsLoginNameExist(USV.LoginName))
+                {
+                    UM.AddUserAccount(USV);
+                    FormsAuthentication.SetAuthCookie(USV.FirstName, false);
+                    return RedirectToAction("Welcome", "Home");
+
+                }
+                else
+                    ModelState.AddModelError("", "Login Name already taken.");
+            }
+            return View();
+        }
+    }
+}
---
+++ modul1/Models/EntityManager/UserManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using modul1.Models.DB;
+using modul1.Models.ViewModel;
+
+namespace modul1.Models.EntityManager
+{
+    public class UserManager
+    {
+        public void AddUserAccount(UserSignUpView user)
+        {
+            using (demoDBEntities db = new demoDBEntities())
+            {
+                SYSUser SU = new SYSUser();
+                SU.LoginName = user.LoginName;
+                SU.PasswordEncryptedText = user.Password;
+                SU.RowCreatedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                SU.RowModifiedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1; ;
+                SU.RowCreatedDateTime = DateTime.Now;
+                SU.RowMOdifiedDateTime = DateTime.Now;
+
+                db.SYSUsers.Add(SU);
+                db.SaveChanges();
+
+                SYSUserProfile SUP = new SYSUserProfile();
+                SUP.SYSUserID = SU.SYSUserID;
+                SUP.FirstName = user.FirstName;
+                SUP.LastName = user.LastName;
+                SUP.Gender = user.Gender;
+                SUP.RowCreatedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                SUP.RowModifiedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                SUP.RowCreatedDateTime = DateTime.Now;
+                SUP.RowModifiedDateTime = DateTime.Now;
+
+                db.SYSUserProfiles.Add(SUP);
+                db.SaveChanges();
+
+                if (user.LOOKUPRoleID > 0)
+                {
+                    SYSUserRole SUR = new SYSUserRole();
+                    SUR.LOOKUPRoleID = user.LOOKUPRoleID;
+                    SUR.SYSUserID = user.SYSUserID;
+                    SUR.IsActive = true;
+                    SUR.RowCreatedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                    SUR.RowModifiedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                    SUR.RowCreatedDateTime = DateTime.Now;
+                    SUR.RowModifiedDateTime = DateTime.Now;
+
+                    db.SYSUserRoles.Add(SUR);
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        public bool IsLoginNameExist(string loginName)
+        {
+            using (demoDBEntities db = new demoDBEntities())
+            {
+                return db.SYSUsers.Where(o => o.LoginName.Equals(loginName)).Any();
+            }
+        }
+    }
+}
